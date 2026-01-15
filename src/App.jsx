ac3f47c594a7e0f60557a54ec6d20a5faf472f0d
Ankammarao Mogalipuvvu <ankammaraomogalipuvvu70@gmail.com>
2026-01-15 12:11:41 +0530

    Update App.jsx
    Local storage updated.

--- src/App.jsx
+++ src/App.jsx
@@ -5,7 +5,7 @@ import { chatbot } from 'supersimpledev';
 import './App.css'
 
 function App() {
-  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem("chatMessages")) || [{
+  const [chatMessages, setChatMessages] = useState(JSON.parse(localStorage.getItem("'messages")) || [{
     message: 'Hello! I am your friendly chatbot. How can I assist you today?',
     sender: 'sender',
     id: 'id1',
@@ -20,9 +20,9 @@ function App() {
       },
       'Who created you?':' I am a sample AI created for demo purposes😊',
       'What is React?':'React is a popular JavaScript library for building user interfaces, particularly single-page applications, developed and maintained by Facebook.',
-     },[])
+     })
          localStorage.setItem('messages', JSON.stringify(chatMessages));
-  })
+  },[ChatMessages])
 
     
 
