dc566991137cfbbaeb8dd03458dc138a8504beab
Ankammarao Mogalipuvvu <ankammaraomogalipuvvu70@gmail.com>
2026-01-31 15:27:14 +0530

    Update user profile image in ChatMessage component

--- src/components/ChatMessage.jsx
+++ src/components/ChatMessage.jsx
@@ -1,5 +1,5 @@
 import RobotProfileImage from '../assets/robot.png'
-import UserProfileImage from '../assets/profile-img.jpg'
+import UserProfileImage from '../assets/profile-1.jpg'
 import './ChatMessage.css'
 function ChatMessage({ message, sender }) 
 {
